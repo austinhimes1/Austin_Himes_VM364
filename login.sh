9e42c7ecd340cc6a05f248fb77b4cbc63ed354be
austinhimes1 <austin_himes@emerson.edu>
2015-02-17 11:29:02 -0500

    initial commit

---
+++ login.sh
@@ -0,0 +1,10 @@
+#unfortunately the lab computers need to have your credentials each time you
+#log on. Download this file and save it on your HD.
+  
+  
+  git config --global user.email "austin_himes@emerson.edu"
+  git config --global user.name "austinhimes1"
+  
+#run these commands in terminal  
+#chmod a+x credentials.sh
+#./credentials.sh
